e79fd6781ce3c2cffedda9d4b04ccf865344f3da
gsofter <real.kingstar@outlook.com>
2021-02-16 18:25:46 -0600

    add datepicker & slot selector

--- frontend/src/views/ReservationModal.vue
+++ frontend/src/views/ReservationModal.vue
@@ -7,9 +7,48 @@
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col cols="12" sm="6" md="4">
+              <v-col cols="12" sm="6" md="4" class="d-flex flex-row">
                 <v-text-field label="Name" required> </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4" class="d-flex flex-row">
                 <v-text-field label="Email" required> </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-select :items="['Foo', 'Bar']" label="Items"></v-select>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-menu
+                  ref="menu"
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :return-value.sync="date"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="auto"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="date"
+                      label="Date"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker v-model="date" no-title scrollable>
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="menu = false">
+                      Cancel
+                    </v-btn>
+                    <v-btn text color="primary" @click="$refs.menu.save(date)">
+                      OK
+                    </v-btn>
+                  </v-date-picker>
+                </v-menu>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-select :items="slots" label="Slots"> </v-select>
               </v-col>
             </v-row>
           </v-container>
@@ -51,6 +90,14 @@ export default {
     saveClick() {
       this.$emit('save')
       // TODO: handle save
+    }
+  },
+
+  data() {
+    return {
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
+      slots: ['01:00 AM - 02:00AM', '02:00AM - 03:00AM', '03:00AM - 04:00AM']
     }
   }
 }
